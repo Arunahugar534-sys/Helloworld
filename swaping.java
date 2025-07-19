6e2081aaf0abc7d8ed9574d7df740450703b1730
Arunahugar534-sys <arunahugar48@gmail.com>
2025-07-19 12:50:07 +0530

    Swpping two numbers

---
+++ swaping.java
@@ -0,0 +1,14 @@
+public class swaping{
+
+public static void main(String[] args){
+int a = 10;
+int b = 15;
+int temp;
+System.out.println("Before swapping the numbers:a = "+ a +",b = " + b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("After swapping the numbers:a = "+ a +", b = " + b);
+}
+}
+
